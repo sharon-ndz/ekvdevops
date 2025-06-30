d2ff291ff2b36dc89db09cdd8927b07627c40702
sharon-ndz <sharon@ndz.co>
2025-06-30 14:00:11 +0000

    ..

--- infra/vpc/provider.tf
+++ infra/vpc/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.2" # Use a known good version
+    }
+  }
+}
