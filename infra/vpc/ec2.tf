d842c10af89e34071388b982ef6279d59b106b15
jbn <jbn434@gmail.com>
2025-07-02 00:53:03 +0530

    Update ec2.tf

--- infra/vpc/ec2.tf
+++ infra/vpc/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = module.vpc.public_subnets_ids[0]
+  subnet_id              = module.vpc.private_subnets_ids[0]
   vpc_security_group_ids = [aws_security_group.open_ssh_and_app.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
   key_name               = var.ec2_key_name
-
+  associate_public_ip_address = false
   user_data = <<-EOF
               #!/bin/bash
               set -e
