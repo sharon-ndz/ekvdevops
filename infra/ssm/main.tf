c55ece8ad2c51810caa1ba8d128930d5f7988b2e
sharon-ndz <sharon@ndz.co>
2025-07-01 08:22:40 +0000

    ..

--- infra/ssm/main.tf
+++ infra/ssm/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "app_env" {
   name        = "/idlms/shared/${var.environment}/.env"
   description = "Shared environment variables for IDLMS in ${var.environment}"
-  type        = "SecureString"
+  type        = "String"
   value       = base64decode(var.app_env_content)
   tags = {
     Environment = var.environment
