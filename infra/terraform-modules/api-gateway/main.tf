e254f1581dd9c6ae83b6f527abae84ac8df39672
Ubuntu <ubuntu@ip-172-31-90-212.ec2.internal>
2025-07-05 07:24:54 +0000

    modified api-gateway

--- infra/terraform-modules/api-gateway/main.tf
+++ infra/terraform-modules/api-gateway/main.tf
@@ -1,93 +1,46 @@
-resource "aws_api_gateway_rest_api" "this" {
-  name = var.api_name
+# --- API Gateway REST API ---
+resource "aws_api_gateway_rest_api" "nestjs_api" {
+  name        = var.api_name
+  description = "API Gateway for NestJS Lambda"
 }
 
+# --- Proxy resource /{proxy+} ---
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.nestjs_api.id
+  parent_id   = aws_api_gateway_rest_api.nestjs_api.root_resource_id
   path_part   = "{proxy+}"
 }
 
+# --- ANY method ---
 resource "aws_api_gateway_method" "proxy_method" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
+  rest_api_id   = aws_api_gateway_rest_api.nestjs_api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.this.id
+# --- Integration with Lambda ---
+resource "aws_api_gateway_integration" "proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.nestjs_api.id
   resource_id             = aws_api_gateway_resource.proxy.id
   http_method             = aws_api_gateway_method.proxy_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.lambda_invoke_arn
-
-  request_templates = {
-    "application/json" = <<EOF
-#set($rawPath = $input.path('$'))
-#set($adjustedPath = $rawPath)
-#if($rawPath == "/api-docs")
-  #set($adjustedPath = "/api-docs/")
-#end
-{
-  "resource": "/{proxy+}",
-  "path": "$adjustedPath",
-  "httpMethod": "$context.httpMethod",
-  "headers": {
-    #foreach($header in $input.params().header.keySet())
-    "$header": "$util.escapeJavaScript($input.params().header.get($header))"
-    #if($foreach.hasNext),#end
-    #end
-  },
-  "queryStringParameters": {
-    #foreach($queryParam in $input.params().querystring.keySet())
-    "$queryParam": "$util.escapeJavaScript($input.params().querystring.get($queryParam))"
-    #if($foreach.hasNext),#end
-    #end
-  },
-  "pathParameters": {
-    "proxy": "$adjustedPath"
-  },
-  "body": $input.body
-}
-EOF
-  }
 }
 
-
-resource "aws_api_gateway_deployment" "this" {
-  depends_on  = [aws_api_gateway_integration.lambda_integration]
-  rest_api_id = aws_api_gateway_rest_api.this.id
+# --- Deployment & stage ---
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.proxy_integration]
+  rest_api_id = aws_api_gateway_rest_api.nestjs_api.id
+  stage_name  = var.stage_name
 }
 
-resource "aws_api_gateway_stage" "this" {
-  deployment_id = aws_api_gateway_deployment.this.id
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  stage_name    = var.stage_name
-}
-
-
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+# --- Lambda permission for API Gateway ---
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/*"
-}
-
-resource "aws_api_gateway_method" "root_any" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_rest_api.this.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "root_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_rest_api.this.root_resource_id
-  http_method             = aws_api_gateway_method.root_any.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.lambda_invoke_arn
+  source_arn    = "${aws_api_gateway_rest_api.nestjs_api.execution_arn}/*/*"
 }
--- infra/terraform-modules/api-gateway/outputs.tf
+++ infra/terraform-modules/api-gateway/outputs.tf
@@ -1,7 +1,8 @@
 output "api_url" {
-  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/${var.stage_name}"
+  value = "https://${aws_api_gateway_rest_api.nestjs_api.id}.execute-api.${var.region}.amazonaws.com/${var.stage_name}"
 }
 
 output "api_gateway_execution_arn" {
-  value = aws_api_gateway_rest_api.this.execution_arn
+  value = aws_api_gateway_rest_api.nestjs_api.execution_arn
 }
+
