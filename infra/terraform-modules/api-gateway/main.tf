36ac76067e0732bcecb06ad71ce0364df13fd9a7
Ubuntu <ubuntu@ip-172-31-90-212.ec2.internal>
2025-07-08 06:02:05 +0000

    updated stage in main.tf

--- infra/terraform-modules/api-gateway/main.tf
+++ infra/terraform-modules/api-gateway/main.tf
@@ -35,12 +35,6 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.nestjs_api.id
 }
 
-resource "aws_api_gateway_stage" "stage" {
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.nestjs_api.id
-  stage_name    = var.stage_name
-}
-
 
 # --- Lambda permission for API Gateway ---
 resource "aws_lambda_permission" "apigw_invoke" {
