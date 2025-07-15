cb007efda8b5ccea5b11680a80566de2b3bacb3e
sharon-ndz <sharon@ndz.co>
2025-07-15 13:43:22 +0000

    modified securitygroup to open the desired port from dev.tfvars

--- infra/vpc/variables.tf
+++ infra/vpc/variables.tf
@@ -181,3 +181,9 @@ variable "docker_artifact_bucket" {
   type        = string
 }
 
+variable "app_ports" {
+  description = "List of application ports to allow"
+  type        = list(number)
+  default     = [4000] # optional default
+}
+
