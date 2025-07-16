4641bd304e145c7f322dce1050734e393427f096
sharon-ndz <sharon@ndz.co>
2025-07-16 13:20:15 +0000

    modified main.tf

--- infra/terraform-modules/rest-api/main.tf
+++ infra/terraform-modules/rest-api/main.tf
@@ -64,6 +64,27 @@ resource "aws_api_gateway_method" "proxy" {
   }
 }
 
+resource "aws_api_gateway_method" "root" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_rest_api.this.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_rest_api.this.root_resource_id
+  http_method             = aws_api_gateway_method.root.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP"
+  uri                     = "http://${var.nlb_dns_name}:4000/"
+  connection_type         = "VPC_LINK"
+  connection_id           = aws_api_gateway_vpc_link.this.id
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  content_handling        = "CONVERT_TO_BINARY"
+}
+
+
 resource "aws_api_gateway_integration" "proxy" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.proxy.id
