2c52f457e491801c0eb28b8d02545b70bcf5f186
sharon-ndz <sharon@ndz.co>
2025-07-16 10:54:25 +0530

    Update securitygroup.tf

--- infra/vpc/securitygroup.tf
+++ infra/vpc/securitygroup.tf
@@ -2,6 +2,10 @@ resource "aws_security_group" "open_ssh_and_app" {
   name        = "allow_ssh_and_app"
   description = "Allow SSH (22) and App (4000) access from anywhere"
   vpc_id      = module.vpc.vpc_id  # Make sure you pass this variable
+ 
+lifecycle {
+    create_before_destroy = true
+  }
 
   ingress {
     description      = "SSH"
