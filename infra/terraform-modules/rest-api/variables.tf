3cb4a91259c2062d8eff43fcc9b3ae37992f9aa4
sharon-ndz <sharon@ndz.co>
2025-07-16 12:16:33 +0000

    modified

--- infra/terraform-modules/rest-api/variables.tf
+++ infra/terraform-modules/rest-api/variables.tf
@@ -32,3 +32,13 @@ variable "common_tags" {
   description = "Common tags applied to all resources"
   type        = map(string)
 }
+
+variable "vpc_link_target_arns" {
+  description = "List of target ARNs for the VPC Link (e.g., NLB ARN)"
+  type        = list(string)
+}
+
+variable "nlb_dns_name" {
+  description = "DNS name of the NLB used in the integration URI"
+  type        = string
+}
