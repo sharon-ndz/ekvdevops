ca4908f3b052796843d046292a26042fda8ffb98
sharon-ndz <sharon@ndz.co>
2025-07-16 04:48:00 +0000

    modified locals.tf

--- infra/http-api/locals.tf
+++ infra/http-api/locals.tf
@@ -1,10 +1,9 @@
-# locals.tf
 locals {
-  nlb_dns = data.terraform_remote_state.nlb.outputs.nlb_dns_name
+  listener_arns = data.terraform_remote_state.nlb.outputs.nlb_listener_arns
 
   api_routes = {
     for path, port in var.path_to_port_map :
-    path => "http://${local.nlb_dns}:${port}/{proxy}"
+    path => local.listener_arns[port]
   }
 }
 
