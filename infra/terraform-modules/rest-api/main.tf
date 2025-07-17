30edff474f38e9428f66a079aac474a866cfae38
sharon-ndz <sharon@ndz.co>
2025-07-17 13:06:58 +0000

    modified main.tf

--- infra/terraform-modules/rest-api/main.tf
+++ infra/terraform-modules/rest-api/main.tf
@@ -87,6 +87,18 @@ resource "aws_api_gateway_method" "proxy" {
   }
 }
 
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+
 resource "aws_api_gateway_integration" "proxy" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.proxy.id
