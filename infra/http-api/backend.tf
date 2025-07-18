09374243bac1412b595329fe5694a0ef5f1d5f5c
sharon-ndz <sharon@ndz.co>
2025-07-18 05:12:50 +0000

    removed http-api directory

--- infra/http-api/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bckt43"
-    key    = "dev/http-api/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
-}
-
--- infra/http-api/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  listener_arns = data.terraform_remote_state.nlb.outputs.nlb_listener_arns
-
-  api_routes = {
-    for path, port in var.path_to_port_map :
-    path => local.listener_arns[port]
-  }
-}
-
