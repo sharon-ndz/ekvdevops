7e67d3f0d3d235a06b8bd889fe94b3932a433a2a
sharon-ndz <sharon@ndz.co>
2025-07-21 12:08:24 +0000

    Removed Ec2 and DB

--- infra/ec2/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "lambda" {
-  backend = "s3"
-  config = {
-    bucket = "back-up-tester"
-    key    = "dev/lambda/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
