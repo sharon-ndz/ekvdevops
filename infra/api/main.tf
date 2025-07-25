012a1a2a332e54762f0d51b5eb59beb0743edc6c
sharon-ndz <sharon@ndz.co>
2025-07-25 10:37:26 +0000

    modified main.tf in api

--- infra/api/main.tf
+++ infra/api/main.tf
@@ -2,8 +2,8 @@ module "api_gateway" {
   source                = "../terraform-modules/api-gateway"
   api_name              = var.api_name
   stage_name            = var.stage_name
-  lambda_function_name  = data.terraform_remote_state.lambda.outputs.lambda_function_name
-  lambda_invoke_arn     = data.terraform_remote_state.lambda.outputs.lambda_function_invoke_arn
+  lambda_function_name  = data.terraform_remote_state.lambda-idlms.outputs.function_name
+  lambda_invoke_arn     = data.terraform_remote_state.lambda-idlms.outputs.function_invoke_arn
   region                = var.region
   log_group_name        = var.log_group_name
 }
