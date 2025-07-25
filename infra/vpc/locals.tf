7c595a1e36e4b005b1c8bcd03a16f44b0d1947de
sharon-ndz <sharon@ndz.co>
2025-07-25 08:52:15 +0000

    modified main.tf

--- infra/vpc/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  availability_zones     = [for subnet in var.public_subnets : subnet.az]
-  public_subnet_cidrs    = [for subnet in var.public_subnets : subnet.cidr_block]
-  private_subnet_cidrs   = [for subnet in var.private_subnets : subnet.cidr_block]
-}
--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -2,9 +2,9 @@ module "vpc" {
   source = "../terraform-modules/vpc"
 
   vpc_cidr_block         = var.vpc_cidr_block
-  availability_zones     = local.var.availability_zones
-  public_subnet_cidrs    = local.var.public_subnet_cidrs
-  private_subnet_cidrs   = local.var.private_subnet_cidrs
+  availability_zones     = var.availability_zones
+  public_subnet_cidrs    = var.public_subnet_cidrs
+  private_subnet_cidrs   = var.private_subnet_cidrs
   project_name           = var.project_name
   tags                   = var.tags
 }
