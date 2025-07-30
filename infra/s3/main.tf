be5b8b5442da7913d2de7d1828a9588eb3563c6a
sharon-ndz <sharon@ndz.co>
2025-07-30 23:37:17 +0530

    Update main.tf

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -6,9 +6,7 @@ resource "aws_s3_bucket" "docker_backup" {
     Environment = var.environment
   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  
 }
 
 resource "aws_s3_bucket_versioning" "docker_backup_versioning" {
