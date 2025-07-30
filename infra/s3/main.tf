8f134515b15e9a4c28337ae677ac73edc7296199
sharon-ndz <sharon@ndz.co>
2025-07-30 18:00:40 +0530

    Update main.tf

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -11,6 +11,17 @@ resource "aws_s3_bucket" "docker_backup" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "docker_backup_block" {
+  bucket = aws_s3_bucket.docker_backup.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
 resource "aws_s3_bucket_versioning" "docker_backup_versioning" {
   bucket = aws_s3_bucket.docker_backup.id
 
