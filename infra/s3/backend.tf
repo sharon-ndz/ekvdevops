335c7384023e2c712cddc790c28f27bf43615e3d
sharon-ndz <sharon@ndz.co>
2025-07-30 16:10:46 +0530

    Update backend.tf

--- infra/s3/backend.tf
+++ infra/s3/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bckt43"      
+    bucket         = "my-terraform-state-bckt432"      
     key            = "dev/s3/terraform.tfstate"          
     region         = "us-east-1"
     encrypt        = true
