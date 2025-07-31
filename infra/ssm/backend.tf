1a0e4f07b810ef426a17c86fbd84a24276b07ee4
sharon-ndz <sharon@ndz.co>
2025-07-31 14:42:22 +0530

    Update backend.tf for ssm

--- infra/ssm/backend.tf
+++ infra/ssm/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bckt4321"      
+    bucket         = "stage-btl-idlms-backend-api-tfstate"      
     key            = "stage/ssm/terraform.tfstate"          
     region         = "eu-west-1"
     encrypt        = true
