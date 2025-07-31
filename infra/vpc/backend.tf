abec8a2cd93824847c38462597d02c164f1309b6
sharon-ndz <sharon@ndz.co>
2025-07-31 14:41:30 +0530

    Update backend.tf

--- infra/vpc/backend.tf
+++ infra/vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-state-bckt4321"
-    key    = "stage/vpc/terraform.tfstate"
+    key    = "stage-btl-idlms-backend-api-tfstate"
     region = "eu-west-1"
     encrypt = true
   }
