0beaa56e8b84d43ad5f84c04ca0ee917d28a0e8e
sharon-ndz <sharon@ndz.co>
2025-07-31 15:03:10 +0530

    Update backend.tf
    updated bucket name and path

--- infra/vpc/backend.tf
+++ infra/vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bckt4321"
-    key    = "stage-btl-idlms-backend-api-tfstate"
+    bucket = "stage-btl-idlms-backend-api-tfstate"
+    key    = "stage/vpc/terraform.tfstate"
     region = "eu-west-1"
     encrypt = true
   }
