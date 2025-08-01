e8727200f6434b5a50951e4dea793e69d88ff5d5
sharon-ndz <sharon@ndz.co>
2025-08-01 11:06:36 +0530

    Update variables.tf
    removed variable named bucket name

--- infra/s3/variables.tf
+++ infra/s3/variables.tf
@@ -3,11 +3,6 @@ variable "environment" {
   type        = string
 }
 
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for Docker artifact backups"
-  type        = string
-}
-
 variable "enable_versioning" {
   description = "Enable versioning on the S3 bucket"
   type        = bool
