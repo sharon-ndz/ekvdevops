4ee061d04c0f13e73a064f9d70f49f77a373edff
sharon-ndz <sharon@ndz.co>
2025-08-01 10:16:03 +0530

    Update main.tf

--- infra/terraform-modules/cloudwatch/main.tf
+++ infra/terraform-modules/cloudwatch/main.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_cloudwatch_log_group" "docker_api" {
   name              = var.docker_log_group_name
   retention_in_days = var.retention_in_days
-  skip_destroy      = true
+
 
   tags = {
     Name = var.log_group_tag_name
