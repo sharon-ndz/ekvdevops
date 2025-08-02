0d4e2f4c0934ec9be21e35bf7ee65ea3ba1ef4ea
sharon-ndz <sharon@ndz.co>
2025-08-02 21:13:54 +0530

    Update main.tf

--- infra/ssm/main.tf
+++ infra/ssm/main.tf
@@ -3,7 +3,7 @@ module "app_env" {
   name        = var.ssm_param_name
   description = var.ssm_param_description
   type        = "String"
-  value       = base64decode(var.app_env_content)
+  value       = var.app_env_content
   tags = {
     Environment = var.environment
     App         = var.ssm_param_app_tag
