5518f6e2d94057cfcf52b66d6a66aecd1ce50d39
sharon-ndz <sharon@ndz.co>
2025-08-02 18:04:16 +0530

    Update data.tf

--- infra/nlb/data.tf
+++ infra/nlb/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "var.tf_state_bucket"
-    key    = "var.tf_state_key"
+    key    = "${var.environment}/nlb/terraform.tfstate"
     region = "eu-west-1"
   }
 }
