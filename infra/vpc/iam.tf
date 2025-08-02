1d46843c3cc72b68b3c834cdcc7e4fb0370b0856
sharon-ndz <sharon@ndz.co>
2025-08-02 20:42:22 +0530

    Update iam.tf

--- infra/vpc/iam.tf
+++ infra/vpc/iam.tf
@@ -32,32 +32,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
   policy_arn = var.cloudwatch_agent_policy_arn # "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-#  Docker Artifact S3 Read Access
-resource "aws_iam_policy" "s3_docker_backup_access" {
-  name = "${var.environment}-ec2-s3-docker-backup-access"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          "arn:aws:s3:::${var.docker_artifact_bucket}",
-          "arn:aws:s3:::${var.docker_artifact_bucket}/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "s3_docker_backup_access" {
-  role       = aws_iam_role.ec2_ssm_role.name
-  policy_arn = aws_iam_policy.s3_docker_backup_access.arn
-}
 
 #  ECR Pull Access
 resource "aws_iam_policy" "ecr_pull" {
