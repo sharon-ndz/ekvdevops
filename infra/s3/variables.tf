c615988c3fc3ac5eff08ea4e206b8e5e00ad3ff3
sharon-ndz <sharon@ndz.co>
2025-08-02 21:31:46 +0530

    Update variables.tf

--- infra/s3/variables.tf
+++ infra/s3/variables.tf
@@ -3,10 +3,6 @@ variable "environment" {
   type        = string
 }
 
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for Docker artifact backups"
-  type        = string
-}
 
 variable "enable_versioning" {
   description = "Enable versioning on the S3 bucket"
