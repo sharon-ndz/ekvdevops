858308817f5cf62e6454df0b1516f7e4a9b64fbd
sharon-ndz <sharon@ndz.co>
2025-08-02 23:08:44 +0530

    Update main.tf

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "artifact" {
-  bucket = "idlms-${var.environment}-built-artifact-${data.aws_caller_identity.current.account_id}"
+  bucket = "idlms-built-artifact-${data.aws_caller_identity.current.account_id}"
 
   tags = {
     Name        = "IDLMS ${var.environment} Artifact Bucket"
