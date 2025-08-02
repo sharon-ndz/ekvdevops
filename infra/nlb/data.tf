3a4a8073e002746dc01cf7541ee470e1890b9a92
sharon-ndz <sharon@ndz.co>
2025-08-02 17:08:58 +0530

    Update data.tf

--- infra/nlb/data.tf
+++ infra/nlb/data.tf
@@ -1,9 +1,9 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = idlms-terraformstate-dev-574643567490
+    bucket = "idlms-terraformstate-dev-574643567490"
     key    = "dev/vpc/terraform.tfstate"
-    region = eu-west-1
+    region = "eu-west-1"
   }
 }
 
